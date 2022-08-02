666fbceae7ad7960ba041581630b1fd3304cdf55
Tatyana <android87@list.ru>
2022-08-02 15:46:01 +0400

    task6 revised

--- task6.py
+++ task6.py
@@ -2,7 +2,7 @@
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
 def get_sequence(n):
-    return [-3 **i for i in range(1, n+1)]
+    return [(-3) **i for i in range(1, n+1)]
 answer = get_sequence(5)
 print(answer)
 
